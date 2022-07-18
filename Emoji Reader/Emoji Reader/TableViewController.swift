f5c6198c1986c5d2c67af1b086ff1cd922149ec4
macbook <yevhen.biiak@icloud.com>
2022-07-18 17:24:21 +0300

    implement dataSource methods, add navigationController, add editButton to navigationController, set title

--- Emoji Reader/Emoji Reader/TableViewController.swift
+++ Emoji Reader/Emoji Reader/TableViewController.swift
@@ -11,9 +11,34 @@ class TableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: UITableViewCell.cellIdentifier)
+        
+        title = "Emoji Reader"
+        navigationItem.leftBarButtonItem = editButtonItem
     }
-
-
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: UITableViewCell.cellIdentifier, for: indexPath)
+        
+        var config = cell.defaultContentConfiguration()
+        config.text = indexPath.description
+        cell.contentConfiguration = config
+        return cell
+    }
 }
+
+
+
+protocol IdentifiableCell {}
+extension IdentifiableCell { static var cellIdentifier: String { String(describing: self) }}
+extension UITableViewCell: IdentifiableCell {}
 
